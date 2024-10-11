8cab9d7f7e710b5e6ef1228c1d605781593b9c38
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-10-11 12:06:14 +0530

    initialize native app

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold text-red-500">
+        Hello there brother
+      </Text>
+    </View>
+  );
+}
