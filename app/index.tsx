6df2798e1060cf94519afc2185dcbcd0319342ed
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-10-11 13:20:47 +0530

    add fonts and images

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-bold text-red-500">
-        Hello there brother
-      </Text>
+      <Text className="text-3xl text-red-500 font-pblack">Hello there</Text>
+      <Link href={"/profile"} className="text-blue-500">
+        Go to profile
+      </Link>
     </View>
   );
 }
