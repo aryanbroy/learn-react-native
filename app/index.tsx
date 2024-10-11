4f19697a7159aa0e87f4a51adf7f19ef73589c2e
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-10-11 13:53:54 +0530

    create tabs and navigation functionality

--- app/index.tsx
+++ app/index.tsx
@@ -6,8 +6,8 @@ export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-3xl text-red-500 font-pblack">Hello there</Text>
-      <Link href={"./(tabs)/profile"} className="text-blue-500">
-        Go to profile
+      <Link href={"/home"} className="text-blue-500">
+        Go to home
       </Link>
     </View>
   );
