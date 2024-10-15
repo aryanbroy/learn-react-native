b0b0d4fb03ac95a4de41a84b384ce1bc779e40ce
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-10-15 17:34:40 +0530

    create sign in page

---
+++ components/FormField.tsx
@@ -0,0 +1,57 @@
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { icons } from "@/constants";
+import CustomButton from "./CustomButton";
+
+export default function FormField({
+  title,
+  value,
+  handleChangeText,
+  otherStyles,
+  keyboardType,
+  placeholder,
+}: {
+  title: string;
+  value: string;
+  handleChangeText: (e: string) => void;
+  otherStyles?: string;
+  keyboardType?: string;
+  placeholder?: string;
+}) {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor={"#7B7B8B"}
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+        />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={showPassword ? icons.eyeHide : icons.eye}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
